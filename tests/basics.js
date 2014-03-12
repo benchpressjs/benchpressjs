14d65fffd3e6d081df3e72723931626e56745144
psychobunny <psycho.bunny@hotmail.com>
2014-03-12 19:51:57 -0400

    init

---
+++ tests/basics.js
@@ -0,0 +1,30 @@
+"use strict";
+/*global templates, json*/
+
+templates.registerHelper('canspeak', function(data, iterator, numblocks) {
+	return (data.isHuman && data.name === "Human") ? "Can speak" : "Cannot speak";
+});
+
+function start() {
+	document.getElementById('reset').reset();
+
+	var tests = document.getElementsByClassName('unit');
+
+	for (var t in tests) {
+		if (tests.hasOwnProperty(t)) {
+			var test = tests[t],
+				template = (test.querySelector('.template').value).toString(),
+				expectEl = test.querySelector('.expect');
+
+			test.querySelector('.result').value = templates.parse(json, template);
+
+			if (test.querySelector('.result').value === expectEl.value) {
+				expectEl.style.border = "2px solid green";
+			} else {
+				expectEl.style.border = "2px solid red";
+			}
+		}
+	}
+}
+
+window.onload = start;
