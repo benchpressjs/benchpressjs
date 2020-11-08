dadbb52f670d74eb90f36fae8fca97325328657b
Peter Jaszkowiak <p.jaszkow@gmail.com>
2020-11-08 01:11:42 -0700

    Add deprecation console warnings

--- tests/precompile.spec.js
+++ tests/precompile.spec.js
@@ -20,4 +20,24 @@ describe('precompile', () => {
         assert(code.length);
       })
   );
+
+  it('should work with callback usage', (done) => {
+    Benchpress.precompile(template, {}, (err, code) => {
+      assert.ifError(err);
+
+      assert(code);
+      assert(code.length);
+      done();
+    });
+  });
+
+  it('should work with old arguments', (done) => {
+    Benchpress.precompile({ source: template }, (err, code) => {
+      assert.ifError(err);
+
+      assert(code);
+      assert(code.length);
+      done();
+    });
+  });
 });
